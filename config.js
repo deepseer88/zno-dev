ed18aaef7478b8e7f585166d98c76fdc0a17d471
Yulia-a <yulia.a.a@outlook.com>
2016-05-10 16:04:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "https://zno-dev.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "YQJlVPIIW2c9QEnLWgxquFKUW9WpShWDYTi52LL9E0HNCR84qRt3OMYXxlPel76duxZjhcpgYoY5pO3PsIQ5dQ==";
+config.host = process.env.HOST || "";
+config.authKey = process.env.AUTH_KEY || "";
 config.databaseId = "ZnoDB";
 config.collectionId = "ZnoColl";
 
